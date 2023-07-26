2dd992117737f3c4fa969b1f4015012c63b6a669
Zafir Abdullah <63918801+Zafir547@users.noreply.github.com>
2023-07-26 20:46:06 +0500

    update hello-world-express-server

---
+++ server.mjs
@@ -0,0 +1,27 @@
+// importing package
+import express from "express";
+
+// initializing express app
+const app = express();
+
+// get request from http://localhost:3000/
+app.get("/", (req, res) => {
+    console.log("🚀 ~ file: server.mjs:7 ~ app.get ~ req:")
+    res.send({ message: "Hello World! by Zafir"});
+});
+
+// get request from http://localhost:3000/name/Zafir
+app.get("/name/:myName", (req, res) =>{
+    const name = req.params.myName;
+
+    console.log("🚀 ~ file: server.mjs:13 ~ app.get ~ name:", name);
+
+    res.send({ message: `new Hello World! by ${name}` });
+});
+
+const port = process.env.PORT || 3000;
+
+// app is listening from here
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
